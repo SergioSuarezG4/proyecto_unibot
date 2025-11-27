5c503fe47a5ea77b0ba32a491c077e7228a0378d
SergioSuarezG4 <chechonieto235@gmail.com>
2025-11-27 17:36:00 -0500

    Se añade manejo de ruta al proyecto

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,10 +3,13 @@ import NavBar from "../components/layout/NavBar.jsx"
 import Layout from "../components/layout/Layout.jsx";
 import Hero from "../components/sections/landing/Hero.jsx";
 import HomePage from "../pages/HomePage.jsx";
+import ChatbotPage from "../pages/ChatbotPage.jsx";
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/chat" element={<ChatbotPage/>}>
+                </Route>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<HomePage/>}/>
                 </Route>
